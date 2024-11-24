df1b94156590b89b778bdf60eddb70f8d30b200e
Kanishk Singh <130019754+KanishkS1ngh@users.noreply.github.com>
2024-11-23 20:39:50 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,16 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/templates/'
+STATIC_URL = '/static/'
 
 # The directory where collectstatic will collect static files for deployment
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Additional locations the staticfiles app will traverse
 STATICFILES_DIRS = [
-    BASE_DIR / 'templates',
+    BASE_DIR / 'static',
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
